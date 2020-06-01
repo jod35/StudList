43954e3d1d53f1728439a193d4c216e5a1ffffc3
jod35 <jodestrevin@gmail.com>
2020-06-01 09:28:10 -0400

    second commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const first_name=document.querySelector("#first_name").value;
+const last_name=document.querySelector('#lastname').value;
+const age=document.querySelector("#age").value;
+const gender=document.querySelector("#gender").value;
+const grade=document.querySelector("#grade").value;
+
+
+// const add_button=document.querySelector("#submit");
+const form =document.querySelector('form');
+
+
+form.addEventListener("submit",submitListItem);
+
+
+function submitListItem(e){
+
+    
+    
+    
+    e.preventDefault();
+}
+
+
